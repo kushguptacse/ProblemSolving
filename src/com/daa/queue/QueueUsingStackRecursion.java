3222d4b4bcba599192f50aaec95fb1cf7f117b9a
G521885 <G521885@NDANB00001104.mph.morpho.com>
2019-07-29 10:45:55 +0530

    Implement queue using single stack added.

---
+++ src/com/daa/queue/QueueUsingStackRecursion.java
@@ -0,0 +1,84 @@
+package com.daa.queue;
+
+import java.util.Stack;
+
+/**
+ * Insert data in the FIFO order implement Queue using stack.
+ * 
+ * @f:off
+ * we take one stack. and for other stack we will method call stack.
+ *  
+ * Insert element in stack1. 
+ * on delete - if stack.size==1 return stack.pop.
+ * store stack.pop() into item and call poll again. after that add item back to stack. 
+ * @f:on
+ * 
+ * @author G521885
+ *
+ * @param <T>
+ */
+public class QueueUsingStackRecursion<T extends Comparable<T>> {
+
+	public static void main(String[] args) {
+		QueueUsingStackRecursion<Integer> obj = new QueueUsingStackRecursion<>();
+		obj.add(1);
+		obj.add(2);
+		obj.add(3);
+		System.out.println("size "+obj.size());
+		System.out.println("poll : "+ obj.poll());
+		System.out.println("size "+obj.size());
+		System.out.println("poll : "+ obj.poll());
+		System.out.println("poll : "+ obj.poll());
+		System.out.println("poll : "+ obj.poll());
+		obj.add(4);
+		System.out.println("poll : "+ obj.poll());
+		obj.add(5);
+		obj.add(6);
+		System.out.println("poll : "+ obj.poll());
+		System.out.println("poll : "+ obj.poll());
+		System.out.println("size "+obj.size());
+	}
+
+	private Stack<T> stack1 = new Stack<>();
+
+	/**
+	 * add the data to rear of queue- o(logn)
+	 * 
+	 * @param data
+	 */
+	public void add(T data) {
+		stack1.push(data);
+	}
+
+	/**
+	 * remove the data from front - o(logn)
+	 * 
+	 * @return data
+	 */
+	public T poll() {
+		if (stack1.isEmpty()) {
+			return null;
+		}
+		if (stack1.size() == 1) {
+			return stack1.pop();
+		}
+		T item = stack1.pop();
+		T result = poll();
+		stack1.push(item);
+		return result;
+	}
+
+	/**
+	 * o(1)
+	 * 
+	 * @return true if empty
+	 */
+	public boolean isEmpty() {
+		return stack1.isEmpty();
+	}
+
+	public int size() {
+		return stack1.size();
+	}
+
+}
