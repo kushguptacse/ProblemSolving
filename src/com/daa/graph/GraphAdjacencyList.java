1a451d81d627c4bc8dac0c942fdb141dc64a86f2
G521885 <G521885@NDANB00001104.mph.morpho.com>
2019-07-30 15:45:57 +0530

    Graph Adjacency List implementation completed. add Edge added

--- src/com/daa/graph/GraphAdjacencyList.java
+++ src/com/daa/graph/GraphAdjacencyList.java
@@ -3,7 +3,32 @@ package com.daa.graph;
 import com.daa.list.Node;
 
 /**
+ * Graph adjacency List implementation of graph -
  * 
+ * @f:off
+ * prepare array of List. where array size is v. where v is the number of vertices.
+ * addEdge - i,j. we will use i as index and add j to end of the list to that index.
+ * 
+ * for e.g. - 
+ * if 0 vertex is connected to 1 vertex. set array[0][1]=true;
+ * 
+ * input- 
+ * 0 - 3
+ * |\
+ * | 2
+ * |/ 
+ * 1 
+ * 
+ * output-
+ * 0 ->1->2->3
+ * 1 ->0->2
+ * 2 ->0->1
+ * 3 ->0
+ * 
+ * @f:on
+ * 
+ * cons: Removal is very complex. to check whether an edge exist between two vertices can take o(n) time.
+ * pros: In case of sparse graph (less edges) it will take less space. Saves space O(|V|+|E|). Adding a vertex is easier.
  *
  */
 public class GraphAdjacencyList {
@@ -21,12 +46,28 @@ public class GraphAdjacencyList {
 	}
 
 	/**
-	 * TODO
+	 * 
 	 * @param i
 	 * @param j
 	 */
 	public void addEdge(int i, int j) {
+		if (i >= 0 && j >= 0 && i < noOfVertices && j < noOfVertices) {
+			add(i, j);
+			add(j, i);
+		}
+	}
 
+	private void add(int x, int y) {
+		Node<Integer> node = adjListArray[x];
+		Node<Integer> node1 = new Node<>(y);
+		if (node == null) {
+			adjListArray[x] = node1;
+		} else {
+			while (node.getNext() != null) {
+				node = node.getNext();
+			}
+			node.setNext(node1);
+		}
 	}
 
 	/**
@@ -39,7 +80,9 @@ public class GraphAdjacencyList {
 			Node<Integer> temp = adjListArray[i];
 			while (temp != null) {
 				System.out.print("->" + temp.getData());
+				temp = temp.getNext();
 			}
+			System.out.println();
 		}
 	}
 
--- src/com/daa/graph/GraphMainApp.java
+++ src/com/daa/graph/GraphMainApp.java
@@ -1,8 +1,32 @@
 package com.daa.graph;
 
+/**
+ * 
+ */
 public class GraphMainApp {
 	public static void main(String[] args) {
+		testAdjacencyList();
 		testAdjacencyMatrix();
+	}
+
+	private static void testAdjacencyList() {
+//		GraphAdjacencyList graph = new GraphAdjacencyList(5);
+//		graph.addEdge(0, 1);
+//		graph.addEdge(0, 4);
+//		graph.addEdge(1, 2);
+//		graph.addEdge(1, 3);
+//		graph.addEdge(1, 4);
+//		graph.addEdge(2, 3);
+//		graph.addEdge(3, 4);
+
+		GraphAdjacencyList g = new GraphAdjacencyList(4);
+		g.addEdge(0, 1);
+		g.addEdge(0, 2);
+		g.addEdge(1, 2);
+		g.addEdge(3, 0);
+
+		// print the adjacency list representation of the above graph
+		g.print();
 	}
 
 	private static void testAdjacencyMatrix() {
@@ -10,8 +34,7 @@ public class GraphMainApp {
 		g.addEdge(0, 1);
 		g.addEdge(0, 2);
 		g.addEdge(1, 2);
-		g.addEdge(2, 0);
-		g.addEdge(2, 3);
+		g.addEdge(3, 0);
 		g.print();
 	}
 }
