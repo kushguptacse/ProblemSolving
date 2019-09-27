322de9cf4ef993559099f3d5d0140c18d1d1987a
g521885 <Kush.GUPTA@idemia.com>
2019-09-27 16:03:47 +0530

    Quick sort added for pivot as first element

--- src/com/daa/algo/SortUtil.java
+++ src/com/daa/algo/SortUtil.java
@@ -2,19 +2,69 @@ package com.daa.algo;
 
 import java.util.Arrays;
 
-public class SortUtil {
+/**
+ * Stable algo - Stable sorting algorithms maintain the relative order of
+ * records with equal keys (i.e. values). That is, a sorting algorithm is stable
+ * if whenever there are two records R and S with the same key and with R
+ * appearing before S in the original list, R will appear before S in the sorted
+ * list
+ * 
+ * In-place algorithm is an algorithm which transforms input using no auxiliary
+ * data structure. However a small amount of extra storage space is allowed for
+ * auxiliary variables
+ * 
+ * An adaptive algorithm is an algorithm that changes its behavior at the time
+ * it is run, based on information available and on a priori defined reward
+ * mechanism. e.g. shell sort. it takes into consideration some part of data
+ * already sorted. and hence work faster on such cases where such type of chunk
+ * exists
+ * 
+ * @author G521885
+ *
+ */
+public final class SortUtil {
 
 	private SortUtil() {
 		throw new RuntimeException("object creation not allowed");
 	}
 
 	public static void main(String[] args) {
-		int[] ar = new int[] { 1, 3, 5, 2, 4 };
+		int[] ar = new int[] { 15, 17, 13, 16, 14 };
+		System.out.println("Quick sort -> ");
+		quickSort(ar);
+		System.out.println(Arrays.toString(ar));
+		ar = new int[] { 15, 12, 13, 11, 20, 15, 22, 14 };
+		System.out.println("Quick sort -> ");
+		quickSort(ar);
+		System.out.println(Arrays.toString(ar));
+
+		ar = new int[] { 6, 5, 9, 0, 8, 2, 4, 7 };
+		System.out.println("Quick sort - ");
+		quickSort(ar);
+		System.out.println(Arrays.toString(ar));
+
+		ar = new int[] { 10, 7, 8, 9, 1, 5 };
+		System.out.println("Quick sort = ");
+		quickSort(ar);
+		System.out.println(Arrays.toString(ar));
+
+		ar = new int[] { 5, 0, 4, 3, 2, 11 };
+		System.out.println("Quick sort : ");
+		quickSort(ar);
+		System.out.println(Arrays.toString(ar));
+
+		System.out.println("Insertion sort");
+		insertionSort(new Integer[] { 34, -3, 0, 2, 14, 8, -1, 24 });
+		System.out.println(Arrays.toString(ar));
+		System.out.println("Merge sort");
 		mergeSort(ar);
 		System.out.println(Arrays.toString(ar));
 	}
 
 	/**
+	 * 
+	 * In-place,I,stable
+	 * 
 	 * O(n^2) - Array after sort
 	 * 
 	 * bubble sort - compare 0 element with 1 and arrange them. then it take 1
@@ -22,14 +72,14 @@ public class SortUtil {
 	 * the end of array. from next iteration onwards we will start from 0 and ignore
 	 * last element as it is already in the correct position.
 	 * 
-	 * @param t
+	 * @param array
 	 */
-	public static <T extends Comparable<T>> void bubbleSort(T[] t) {
-		for (int i = 0; i < t.length - 1; i++) {
+	public static <T extends Comparable<T>> void bubbleSort(T[] array) {
+		for (int i = 0; i < array.length - 1; i++) {
 			boolean swap = false;
-			for (int j = 0; j < t.length - i - 1; j++) {
-				if (t[j].compareTo(t[j + 1]) > 0) {
-					swap(t, j, j + 1);
+			for (int j = 0; j < array.length - i - 1; j++) {
+				if (array[j].compareTo(array[j + 1]) > 0) {
+					swap(array, j, j + 1);
 					swap = true;
 				}
 			}
@@ -40,6 +90,8 @@ public class SortUtil {
 	}
 
 	/**
+	 * In-place,I
+	 * 
 	 * O(n^2)
 	 * 
 	 * selection sort - find minimum element from 0 to n first and then swap 0th
@@ -52,15 +104,15 @@ public class SortUtil {
 	 * 
 	 * @param array to be sorted t
 	 */
-	public static <T extends Comparable<T>> void selectionSort(T[] t) {
-		for (int i = 0; i < t.length; i++) {
+	public static <T extends Comparable<T>> void selectionSort(T[] array) {
+		for (int i = 0; i < array.length; i++) {
 			int minIndex = i;
-			for (int j = i + 1; j < t.length; j++) {
-				if (t[minIndex].compareTo(t[j]) > 0) {
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[minIndex].compareTo(array[j]) > 0) {
 					minIndex = j;
 				}
 			}
-			swap(t, i, minIndex);
+			swap(array, i, minIndex);
 //			System.out.print("After Pass "+(i+1)+" : ");
 //			Arrays.stream(t).forEach(System.out::print);
 //			System.out.println();
@@ -68,9 +120,19 @@ public class SortUtil {
 	}
 
 	/**
+	 * In-place,I,adaptive,stable
+	 * 
 	 * O(n^2)
 	 * 
 	 * insertion sort - it is just like playing with cards.
+	 * 
+	 * on small data set insertion sort, selection sort works better than merge or
+	 * quick sort.
+	 * 
+	 * insertion sort requires more swap as compared to selection sort and hence is
+	 * not preferred where write operation is costly. on other hand insertion sort
+	 * is more efficient than bubble or selection as it speeds up when array is
+	 * already partially sorted.
 	 * 
 	 * insert ith element in between 0 to i-1 position where it needed to be
 	 * present. 3,1,2
@@ -79,16 +141,16 @@ public class SortUtil {
 	 * 
 	 * @param array to be sorted t
 	 */
-	public static <T extends Comparable<T>> void insertionSort(T[] t) {
-		for (int i = 1; i < t.length; i++) {
+	public static <T extends Comparable<T>> void insertionSort(T[] array) {
+		for (int i = 1; i < array.length; i++) {
 			for (int j = 0; j < i; j++) {
-				if (t[i].compareTo(t[j]) < 0) {
-					swap(t, i, j);
+				if (array[i].compareTo(array[j]) < 0) {
+					swap(array, i, j);
 				}
 			}
-//			System.out.print("After Pass "+(i)+" : ");
-//			Arrays.stream(t).forEach(s->System.out.print(s+","));
-//			System.out.println();
+			System.out.print("After Pass " + (i) + " : ");
+			Arrays.stream(array).forEach(s -> System.out.print(s + ","));
+			System.out.println();
 		}
 	}
 
@@ -105,6 +167,105 @@ public class SortUtil {
 		a[j] = temp;
 	}
 
+	/**
+	 * Ravindra,geeks
+	 * @f:off
+	 * 
+	 * In-place,Not Stable,Not adaptive,Divide and conquer
+	 * 
+	 * Perform quick sort algo to sort the data. for primitive it is preferred and for objects merge sort is preferred
+	 * 
+	 * Steps-
+	 * For every step we will take one element as pivot(here the first one) and will try to put it in right position. 
+	 * by right position we mean that all element to the left of it are small and right to it are greater.
+	 * so, basically we fixing pivot position one by one.
+	 * 
+	 * o(nlogn) - average case o(n^2) - worst case.
+	 * 
+	 * @f:on
+	 * 
+	 * @param arr
+	 */
+	public static void quickSort(int[] arr) {
+		quickSortUtil(arr, 0, arr.length - 1);
+	}
+
+	private static void quickSortUtil(int[] arr, int low, int high) {
+		if (low < high) {
+			int p = partition(arr, low, high);
+			quickSortUtil(arr, low, p - 1);
+			quickSortUtil(arr, p + 1, high);
+		}
+	}
+
+	/**
+	 * @f:off
+	 * 1. select low index element as pivot and i = high+1.
+	 * 2. go from j=high to low-1
+	 * 3. check for every j that whether that element is greater than pivot
+	 * 4. if yes decrement i and swap i with j.
+	 * 
+	 * after loop terminates swap pivot with i-1. and return i-1 which is our new pivot
+	 * e.g.
+	 * 15,17,13,16,14
+	 * p            j,i
+	 * 
+	 * step 1 - 
+	 * 15,17,13,16,14
+	 * p		j    ,i
+	 * 
+	 * 15,17,13,14,16
+	 * p	 j	   i
+	 * 
+	 * 15,17,13,14,16
+	 * p  j        i
+	 * 
+	 * 15,14,13,17,16
+	 * p        i   
+	 * j
+	 * 
+	 * so,now swap p = i-1. and return i-1 i.e. index 2. so, that we will have two partition {13,14} {17,16} left
+	 * 
+	 * 
+	 * 
+	 * @f:on
+	 * 
+	 * @param arr
+	 * @param low
+	 * @param high
+	 * @return new pivot
+	 */
+	private static int partition(int[] arr, int low, int high) {
+		int pivot = low;
+		int i = high + 1;
+		for (int j = high; j > low; j--) {
+			if (arr[j] > arr[pivot]) {
+				i--;
+				swapIntArray(arr, i, j);
+			}
+		}
+		swapIntArray(arr, pivot, i - 1);
+		return i - 1;
+	}
+
+	/**
+	 * swap i index element with j index element
+	 * 
+	 * @param a
+	 * @param i
+	 * @param j
+	 */
+	private static void swapIntArray(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+
+	/**
+	 * Divide and conquer,R,stable algo,not in-place,not adaptive, nlogn
+	 * 
+	 * @param arr
+	 */
 	public static void mergeSort(int[] arr) {
 		mergeSort(arr, 0, arr.length - 1);
 	}
