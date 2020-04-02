99f12ada4a3b6b252c5ba179227f661ba6bf2c54
g521885 <Kush.GUPTA@idemia.com>
2020-04-02 16:40:23 +0530

    happy number added

--- src/com/daa/math/MathUtil.java
+++ src/com/daa/math/MathUtil.java
@@ -167,8 +167,59 @@ public class MathUtil {
 	 */
 	public static boolean checkPalindrome(int num) {
 		int num1 = num;
-		int res = reverse(num);		
+		int res = reverse(num);
 		return num1 == res;
 	}
 
+	/**
+	 * Write an algorithm to determine if a number is "happy".
+	 * 
+	 * A happy number is a number defined by the following process: Starting with
+	 * any positive integer, replace the number by the sum of the squares of its
+	 * digits, and repeat the process until the number equals 1 (where it will
+	 * stay), or it loops endlessly in a cycle which does not include 1. Those
+	 * numbers for which this process ends in 1 are happy numbers.
+	 * 
+	 * @f:off
+	 * Input: 19
+	 * Output: true
+     * Explanation: 
+     * 12 + 92 = 82
+     * 82 + 22 = 68
+     * 62 + 82 = 100
+     * 12 + 02 + 02 = 1
+	 * @f:on
+	 * 
+	 * Approach - 
+	 * 1. Loop infinite and find squareAndSum of n.
+	 * 2. If n reach to 7 or 1. it means it is happy number as 7 will also resolve to 1.
+	 * 3. any other single number digit will never resolve to 1 or 7. 
+	 * and hence we return false in such case
+	 * 
+	 * 
+	 * @param n
+	 * @return true if number ends to 1
+	 */
+	public boolean isHappy(int n) {
+		if (n < 1) {
+			return false;
+		}
+		while (true) {
+			n = squareAndSum(n);
+			if (n == 1 || n == 7)
+				return true;
+			if (n < 10)
+				return false;
+		}
+	}
+
+	private int squareAndSum(int n) {
+		int sum = 0;
+		while (n != 0) {
+			int rem = n % 10;
+			n = n / 10;
+			sum = sum + (rem * rem);
+		}
+		return sum;
+	}
 }
