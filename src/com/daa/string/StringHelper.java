ded434f724d43bb999758f9e524d900d907a128a
g521885 <Kush.GUPTA@idemia.com>
2020-04-15 11:58:59 +0530

    shift string added leetcode

--- src/com/daa/string/StringHelper.java
+++ src/com/daa/string/StringHelper.java
@@ -555,4 +555,77 @@ public final class StringHelper {
 		return h.toString();
 	}
 
+	/**
+	 * Perform String Shifts
+	 * 
+	 * You are given a string s containing lowercase English letters, and a matrix
+	 * shift, where shift[i] = [direction, amount]:
+	 * 
+	 * direction can be 0 (for left shift) or 1 (for right shift). amount is the
+	 * amount by which string s is to be shifted. A left shift by 1 means remove the
+	 * first character of s and append it to the end. Similarly, a right shift by 1
+	 * means remove the last character of s and add it to the beginning.
+	 * 
+	 * Return the final string after all operations.
+	 * 
+	 * @f:off
+	 * Example:
+	 * 
+	 * Input: s = "abcdefg", shift = [[1,1],[1,1],[0,2],[1,3]] 
+	 * 
+	 * Output: "efgabcd"
+	 * 
+	 * Explanation: 
+	 * [1,1] means shift to right by 1. "abcdefg" -> "gabcdef" 
+	 * [1,1] means shift to right by 1. "gabcdef" -> "fgabcde" 
+	 * [0,2] means shift to left by 2. "fgabcde" -> "abcdefg" 
+	 * [1,3] means shift to right by 3. "abcdefg" -> "efgabcd"
+	 * @f:on
+	 * 
+	 * LEETCODE,o(n)
+	 * 
+	 * @param s
+	 * @param shift
+	 * @return string after rotation.
+	 */
+	public static String stringShift(String s, int[][] shift) {
+		int c = 0;
+		for (int i = 0; i < shift.length; i++) {
+			if (shift[i][0] == 0) {
+				c += shift[i][1];
+			} else {
+				c -= shift[i][1];
+			}
+		}
+
+		if (c > 0) {
+			return shiftLeft(c % s.length(), s);
+		}
+		return shiftRight((-c) % s.length(), s);
+	}
+
+	/**
+	 * 
+	 * @param n
+	 * @param s
+	 * @return String after left rotation n
+	 */
+	private static String shiftLeft(int n, String s) {
+		String pre = s.substring(0, n);
+		s = s.substring(pre.length(), s.length());
+		return s + pre;
+	}
+
+	/**
+	 * 
+	 * @param n
+	 * @param s
+	 * @return String after right rotation n
+	 */
+	private static String shiftRight(int n, String s) {
+		String suf = s.substring(s.length() - n, s.length());
+		s = s.substring(0, s.length() - suf.length());
+		return suf + s;
+	}
+
 }
--- src/com/daa/string/StringMainApp.java
+++ src/com/daa/string/StringMainApp.java
@@ -4,25 +4,27 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class StringMainApp {
-	
+
 	public static void main(String[] args) {
+		System.out.println(StringHelper.stringShift("abcdefg", new int[][] { { 1, 1 }, { 1, 1 }, { 0, 2 }, { 1, 3 } }));
+		int[][] a = new int[][] { { 1, 8 }, { 1, 4 }, { 1, 3 }, { 1, 6 }, { 0, 6 }, { 1, 4 }, { 0, 2 }, { 0, 1 } };
+		System.out.println(StringHelper.stringShift("yisxjwry", a));
 		
-		LinkedHashMap<Integer, String> lhm = new LinkedHashMap<Integer,
-			      String>(16 + 1, .75F, false) {
-			         protected boolean removeEldestEntry(Map.Entry<Integer, String> eldest) {
-			            return size() > 16;
-			         }
-			      };
-		String str="radabdar";
-		System.out.println("Is '"+str+"' palindrome : "+StringHelper.checkPalindrome(str));
-		str="radar";
-		System.out.println("Is '"+str+"' palindrome : "+StringHelper.checkPalindrome(str));
-		str="madama";
-		System.out.println("Is '"+str+"' palindrome : "+StringHelper.checkPalindrome(str));
-		str="raar";
-		System.out.println("Is '"+str+"' palindrome : "+StringHelper.checkPalindrome(str));
+		LinkedHashMap<Integer, String> lhm = new LinkedHashMap<Integer, String>(16 + 1, .75F, false) {
+			protected boolean removeEldestEntry(Map.Entry<Integer, String> eldest) {
+				return size() > 16;
+			}
+		};
+		String str = "radabdar";
+		System.out.println("Is '" + str + "' palindrome : " + StringHelper.checkPalindrome(str));
+		str = "radar";
+		System.out.println("Is '" + str + "' palindrome : " + StringHelper.checkPalindrome(str));
+		str = "madama";
+		System.out.println("Is '" + str + "' palindrome : " + StringHelper.checkPalindrome(str));
+		str = "raar";
+		System.out.println("Is '" + str + "' palindrome : " + StringHelper.checkPalindrome(str));
 		System.out.println(StringHelper.longestCommonSubString("neversaynever"));
-		System.out.println(StringHelper.longestCommonPrefix("hello Ji","J"));
+		System.out.println(StringHelper.longestCommonPrefix("hello Ji", "J"));
 		System.out.println(StringHelper.reverse("smellycat"));
 		System.out.println("Suffix are -----------");
 		StringHelper.getSuffixes("apple").forEach(System.out::println);
@@ -38,17 +40,17 @@ public class StringMainApp {
 		s1 = "AABAACAADAABAABA";
 		s2 = "AABA";
 		System.out.println(s2 + " Is Substring of " + s1 + " " + StringHelper.subStringRabinKarpBetter(s1, s2));
-		s1="abcd";
-		s2="bcd";
+		s1 = "abcd";
+		s2 = "bcd";
 		System.out.println(s2 + " Is Substring of " + s1 + " " + StringHelper.subStringRabinKarpBetter(s1, s2));
-		s1="aabcd";
-		s2="acd";
+		s1 = "aabcd";
+		s2 = "acd";
 		System.out.println(s2 + " Is Substring of " + s1 + " " + StringHelper.subStringRabinKarpBetter(s1, s2));
-		s1="aaaaabcd";
-		s2="aabcd";
+		s1 = "aaaaabcd";
+		s2 = "aabcd";
 		System.out.println(s2 + " Is Substring of " + s1 + " " + StringHelper.subStringRabinKarpBetter(s1, s2));
-		s1="aaabcddd";
-		s2="abcd";
+		s1 = "aaabcddd";
+		s2 = "abcd";
 		System.out.println(s2 + " Is Substring of " + s1 + " " + StringHelper.subStringRabinKarpBetter(s1, s2));
 		System.out.println("Length of lcs is: " + StringHelper.longestCommonSubsequence("stone", "longest"));
 		int res = StringHelper.longestCommonSubsequence("AGGTAB", "GXTXAYB");
