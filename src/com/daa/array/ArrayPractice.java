7bf4f89804e769187455c0ef9fc39f7abef88fca
g521885 <Kush.GUPTA@idemia.com>
2020-05-07 17:50:28 +0530

    majority element added

--- src/com/daa/array/ArrayPractice.java
+++ src/com/daa/array/ArrayPractice.java
@@ -20,6 +20,47 @@ public class ArrayPractice {
 	}
 
 	/**
+	 * Given an array of size n, find the majority element. The majority element is
+	 * the element that appears more than ⌊ n/2 ⌋ times.
+	 * 
+	 * You may assume that the array is non-empty and the majority element always
+	 * exist in the array.
+	 * 
+	 * Example 1:
+	 * 
+	 * Input: [3,2,3] 
+	 * 
+	 * Output: 3 
+	 * 
+	 * Example 2:
+	 * 
+	 * Input: [2,2,1,1,1,2,2] 
+	 * 
+	 * Output: 2
+	 * 
+	 * o(n),o(1)
+	 * 
+	 * @param nums
+	 * @return majority element
+	 */
+	public int majorityElement(int[] nums) {
+		int candidate = nums[0];
+		int c = 1;
+		for (int i = 1; i < nums.length; i++) {
+			if (candidate == nums[i]) {
+				c++;
+			} else {
+				c--;
+			}
+			if (c == 0) {
+				candidate = nums[i];
+				c = 1;
+			}
+		}
+		return candidate;
+	}
+
+	/**
 	 * Given an array of non-negative integers, you are initially positioned at the
 	 * first index of the array.
 	 * 
