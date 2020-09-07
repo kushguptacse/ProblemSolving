8c1e7e0e5b3debff2c4245182e7717d3bc59e263
g521885 <Kush.GUPTA@idemia.com>
2020-09-07 11:43:58 +0530

    image overlap added

--- src/com/daa/array/Array2D.java
+++ src/com/daa/array/Array2D.java
@@ -325,4 +325,58 @@ public class Array2D {
 		}
 	}
 
+	/**
+	 * Two images A and B are given, represented as binary, square matrices of the
+	 * same size. (A binary matrix has only 0s and 1s as values.)
+	 * 
+	 * We translate one image however we choose (sliding it left, right, up, or down
+	 * any number of units), and place it on top of the other image. After, the
+	 * overlap of this translation return max number of positions that have a 1 in
+	 * both images overlapping.
+	 * 
+	 * Example 1:
+	 * 
+	 * Input: A = [[1,1,0], [0,1,0], [0,1,0]] B = [[0,0,0], [0,1,1], [0,0,1]]
+	 * Output: 3
+	 * 
+	 * Explanation: We slide A to right by 1 unit and down by 1 unit.
+	 * 
+	 * Notes:
+	 * 
+	 * 1 <= A.length = A[0].length = B.length = B[0].length <= 30 0 <= A[i][j],
+	 * B[i][j] <= 1
+	 * 
+	 * @param a
+	 * @param b
+	 * @return largest overlap possible
+	 */
+	public int largestOverlap(int[][] a, int[][] b) {
+		return Math.max(shiftRightAndBottom(a, b), shiftRightAndBottom(b, a));
+	}
+
+	/**
+	 * @param a
+	 * @param b
+	 * @return max overlapping count
+	 */
+	private int shiftRightAndBottom(int[][] a, int[][] b) {
+		int n = a.length;
+		int max = 0;
+		// go towards right first and then downwards		
+		for (int x = 0; x < n; x++) { // shift in x direction
+			for (int y = 0; y < n; y++) { // shift in y direction
+				int temp = 0;
+				for (int i = x; i < n; i++) { // check i from x to n as other indexes are irrelevant direction
+					for (int j = y; j < n; j++) { // check j from y to n as other indexes are irrelevant direction
+						if (a[i][j] == 1 && b[i - x][j - y] == 1) {
+							temp++;
+						}
+					}
+				}
+				max = Math.max(max, temp);
+			}
+		}
+		return max;
+	}
+
 }
